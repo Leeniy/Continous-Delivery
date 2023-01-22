a74b45dd040ec0ab2909970b45466223c289fb6c
Leeniy <marleen@lh-net.de>
2023-01-22 13:13:45 +0100

    neu projekt

---
+++ src/test/java/com/mlorenz/ContinousDeliveryTest.java
@@ -0,0 +1,11 @@
+package com.mlorenz;
+
+import org.junit.Test;
+
+public class ContinousDeliveryTest{
+
+    @Test
+    public void verifyNoExceptionThrown(){
+        ContinousDelivery.main(new String[]{});
+    }
+}
